e5db4fc0726626ad1ee88604ce090e15d6fe3daa
Maël Robert <contact@mrbt.fr>
2024-05-24 16:27:33 +0200

    feat: login db

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const dbConfig = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'admin',
+  password: process.env.MYSQL_PASSWORD || 'oBImp4Z92nR3SDA',
+  database: process.env.MYSQL_DB || 'doctor'
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows, fields] = await connection.execute('SELECT * FROM user');
+    res.json(rows);
+  } catch (err) {
+    console.error('Error querying the database:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(port, () => {
